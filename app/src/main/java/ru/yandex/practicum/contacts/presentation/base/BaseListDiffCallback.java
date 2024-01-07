95569133d00640fbd868eb4e0103d8bcffcd39b2
altynchach <77624594+altynchach@users.noreply.github.com>
2024-01-07 10:16:52 +0600

    2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -8,19 +8,16 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
 
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        // Используем метод theSameAs из интерфейса ListDiffInterface для сравнения элементов
         return oldItem.theSameAs(newItem);
     }
 
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        // Используем equals для сравнения содержимого элементов
         return oldItem.equals(newItem);
     }
 
     @Override
     public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
-        // В качестве payload возвращаем новый элемент, который может использоваться для определения конкретных изменений
         return newItem;
     }
 }
